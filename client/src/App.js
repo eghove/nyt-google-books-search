84a25c2374a78d92de402654ceb124fdfbfd2952
That_Guy <eghove@gmail.com>
2019-02-22 17:47:12 -0600

    correcting spelling on BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // pull in the React router
-import { BrowerRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 import logo from './logo.svg';
