1d2bc6d5b9286c00a2bd00f4e1b952db34624784
That_Guy <eghove@gmail.com>
2019-02-22 19:39:30 -0600

    jumbotron component and beginning of Search page

---
+++ client/src/pages/Search.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+// import the API from the utils
+import API from "../utils/API";
+// import the Grid components
+import { Container, Row, Column } from "../components/Grid";
