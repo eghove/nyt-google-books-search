1badedd09e530d71c260730b952e19bad3a9d965
That_Guy <eghove@gmail.com>
2019-02-23 07:37:09 -0600

    routing to the 404 no match page done

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
 // import the Search page
 import Search from "./pages/Search";
+// import the No Match page
+import NoMatch from "./pages/NoMatch";
 
 
 // import './App.css';
@@ -16,6 +18,8 @@ function App() {
         <NavBar />
         <Switch>
           <Route exact path = "/" component = {Search} />
+          {/* If no other path matches, load the NoMatch component */}
+          <Route component = {NoMatch} />
 
 
         </Switch>
